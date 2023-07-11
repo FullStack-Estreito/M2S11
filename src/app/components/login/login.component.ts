fc6161d662a970f771f615da04a3eef379d75a49
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-07-11 23:04:08 +0000

    Implementação página de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [ Validators.required, Validators.email ]),
+      'password': new FormControl('', [ Validators.required ])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  validateErrorMessage(field: string) {
+    return this.loginForm.get(field)?.invalid && this.loginForm.get(field)?.touched;
+  }
 }
