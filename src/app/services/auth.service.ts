f9d0da165b92cdf6de4a06951797acf7fe0d33d7
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-07-13 00:56:17 +0000

    Implementação funcionalidade de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,9 +8,28 @@ import { lastValueFrom } from 'rxjs';
 })
 export class AuthService {
 
+  usuarioLogado = false;
+
   constructor(private httpClient: HttpClient) { }
 
   registrar(usuario: IUsuario) {
     return lastValueFrom(this.httpClient.post('http://localhost:3000/usuarios', usuario));
+  }
+
+  async logar(usuario: { email: string, senha: string }) {
+    const usuariosCadastrados = await this._obterUsuarios();
+    for (const usuarioCadastrado of usuariosCadastrados) {
+      const emailValido = usuarioCadastrado.email === usuario.email;
+      const senhaValida = usuarioCadastrado.senha === usuario.senha;
+      if (emailValido && senhaValida) {
+        this.usuarioLogado = true;
+        return;
+      }
+    }
+    throw new Error("Usuário não cadastrado!!");
+  }
+
+  private _obterUsuarios() {
+    return lastValueFrom(this.httpClient.get<IUsuario[]>('http://localhost:3000/usuarios'));
   }
 }
